46c22532c0adda72f6be487337ff936de008a19a
Ryoma0305 <ryomaaa0602@gmail.com>
2022-06-23 11:29:28 +0900

    update session dir

--- session/input.php
+++ session/input.php
@@ -8,15 +8,20 @@ function validate()
 }
 session_start();
 
-if (isset($_POST['operation']) && $_POST['operation'] === 'inquiry') {
+//送信ボタンをクリックしたときは、条件①に入る。条件①でValidate関数で入力データが空欄でないか調べる。空欄があった場合は条件①-Aに入り、input.phpを再表示する。
+//空欄がなければ条件①-Bに入り、セッション変数$_SESSIONに入力データを保存し、confirm.phpに画面遷移する。
+//条件②に入るのは、confirm.phpの「戻る」リンクを使ってinput.phpに戻った時。この処理で、$_SESSIONを元に入力値を復元している。
 
+// 条件①
+if (isset($_POST['operation']) && $_POST['operation'] === 'inquiry') {
+    // 条件①-A
     if (validate() === false) {
         $message = 'メールアドレス・お問い合わせ内容のいずれも必須入力です。';
         $data = [
             'email' => $_POST['email'],
             'message' => $_POST['message']
         ];
-
+    // 条件①-B
     } else {
         $_SESSION['data'] = [
             'email' => $_POST['email'],
@@ -25,7 +30,7 @@ if (isset($_POST['operation']) && $_POST['operation'] === 'inquiry') {
         header('Location: confirm.php');
         return;
     }
-
+// 条件②
 } elseif (isset($_SESSION['data'])) {
     $data = [
         'email' => $_SESSION['data']['email'],
--- session/thankyou.php
+++ session/thankyou.php
@@ -2,6 +2,9 @@
 
 declare(strict_types=1);
 
+//①まず＄_SESSIONの値を空の配列値で上書きする。これでPHPプロセスのメモリ上からはセッション変数の中身が消える。
+//②次にdeleteSession関数を呼び出して、有効期限が過去の日時のセッションクッキーをクライアントに上書き送出することで、PHPSESSIDが書かれたセッションクッキーがクライアントから削除している。
+//③最後にsession_destroy関数を呼び出して、サーバー上の保存されたセッションデータのファイルを削除している。
 function deleteSession()
 {
     if (ini_get('session.use_cookies')) {
@@ -24,8 +27,8 @@ session_start();
     <h2>お問い合わせ完了</h2>
     <p>お問い合わせありがとうございました。デバッグ情報：</p>
     <pre><?php print_r($_SESSION); ?></pre>
-    <?php $_SESSION = []; ?>
-    <?php deleteSession(); ?>
-    <?php session_destroy(); ?>
+    <?php $_SESSION = []; ?>　<!-- ① -->
+    <?php deleteSession(); ?> <!-- ② -->
+    <?php session_destroy(); ?> <!-- ③ -->
 </body>
 </html>

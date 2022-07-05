2fab39cd693db59e64174165a241149793a43595
Ryoma0305 <ryomaaa0602@gmail.com>
2022-07-05 21:24:01 +0900

    update files

--- built-in-functions/count_dim.php
+++ built-in-functions/count_dim.php
@@ -5,4 +5,5 @@ $data = [
   ['Z-1', 'Z-2', 'Z-3']
 ];
 print count($data);
-// print count($data, COUNT_RECURSIVE);
+echo '<br>';
+print count($data, COUNT_RECURSIVE);
--- built-in-functions/implode.php
+++ built-in-functions/implode.php
@@ -1,3 +1,3 @@
 <?php
 $data = ['PHP', 'Perl', 'Ruby', 'Python', 'JavaScript'];
-print implode(',', $data);
+print implode('-', $data);
